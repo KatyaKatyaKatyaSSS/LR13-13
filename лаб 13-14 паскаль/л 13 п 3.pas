a0eb005a54fecc92078f2719881f6db28369431c
Екатерина Созина <118255097+KatyaKatyaKatyaSSS@users.noreply.github.com>
2022-12-15 21:49:40 +0300

    Add files via upload

---
+++ лаб 13-14 паскаль/л 13 п 3.pas
@@ -0,0 +1,15 @@
+﻿begin
+  var s := readstring('s: ');
+  Assign(input, 'C:\Users\Honor\Desktop\лаб 13-14 паскаль\input.txt');
+  Assign(output, 'C:\Users\Honor\Desktop\лаб 13-14 паскаль\output.txt');
+  while not eof(input) do
+  begin
+    var s1 := ReadString;
+    println(s1);
+  end;
+  println(S);
+  close(input); 
+  close(output); 
+  Erase(input);
+  Rename(output, 'input.txt');
+end.
---
+++ лаб 13-14 паскаль/л 14 п 7.pas
@@ -0,0 +1,18 @@
+﻿var
+    f: file of char;
+    c: char;
+    n, i: integer;
+begin
+    assign (f, 'C:\Users\Honor\Desktop\лаб 13-14 паскаль\text.txt');
+    reset (f);
+    c:='!';
+    for i:=0 to filesize(f)-1 do
+      begin
+        if i mod 2 = 1 then
+          begin
+          seek(f,i);
+          write(f,c);
+        end;
+        end;
+    close(f);
+ end.
